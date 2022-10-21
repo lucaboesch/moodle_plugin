b02508efd9a810b7b25b7558cc92ff028c2b4016
Luca Bösch <luca.boesch@bfh.ch>
2022-10-21 08:06:13 +0200

    Omit double headings (and intro) on Moodle ≥ 4.0.0 fixes #385.

--- mod/kalvidassign/view.php
+++ mod/kalvidassign/view.php
@@ -64,49 +64,47 @@ echo $OUTPUT->header();
 
 $renderer = $PAGE->get_renderer('mod_kalvidassign');
 
+if ($CFG->branch < 400) {
+    echo $OUTPUT->heading(format_string($kalvidassign->name, true, array('context' => $context)));
+}
+
 echo $OUTPUT->box_start('generalbox');
 
 echo $renderer->display_mod_info($kalvidassign, $context);
 
+if ($CFG->branch < 400) {
+    echo format_module_intro('kalvidassign', $kalvidassign, $cm->id);
+}
+
 echo $OUTPUT->box_end();
 
-$disabled = $resubmit = false;
+$disabled = false;
 $url = '';
 $width = 0;
 $height = 0;
 
 // Here we can assume that the user has permission to submit no matter what their role is.
 // The old method denied module-level locally assigned instructors the right to submit items,
-// even if they were also students within the course.
+// even if they were also students within the course. 
 $param = array('vidassignid' => $kalvidassign->id, 'userid' => $USER->id);
 $submission = $DB->get_record('kalvidassign_submission', $param);
 
-$displaystudentcontent = has_capability('mod/kalvidassign:submit', $context);
-if ($displaystudentcontent) {
-    echo $renderer->display_video_container_markup($submission, $course->id, $cm->id);
-}
+echo $renderer->display_video_container_markup($submission, $course->id, $cm->id);
 
 if (kalvidassign_assignemnt_submission_expired($kalvidassign)) {
     $disabled = true;
 }
 
-if (!empty($submission->entry_id) && !empty($submission->timecreated)) {
+if (empty($submission->entry_id) && empty($submission->timecreated)) {
+    echo $renderer->display_student_submit_buttons($cm, $USER->id, $disabled);
+    echo $renderer->display_grade_feedback($kalvidassign, $context);
+} else {
     if ($disabled || !$kalvidassign->resubmit) {
         $disabled = true;
     }
-
-    $resubmit = true;
+    echo $renderer->display_student_resubmit_buttons($cm, $USER->id, $disabled);
+    echo $renderer->display_grade_feedback($kalvidassign, $context);
 }
-
-if ($displaystudentcontent) {
-    if ($resubmit) {
-        echo $renderer->display_student_resubmit_buttons($cm, $USER->id, $disabled);
-    } else {
-        echo $renderer->display_student_submit_buttons($cm, $USER->id, $disabled);
-    }
-}
-
-echo $renderer->display_grade_feedback($kalvidassign, $context);
 
 $params = array(
     'withblocks' => 0,
